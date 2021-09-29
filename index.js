66e2274a1e58b28c2635773dc49f3cd81ed5def9
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2021-09-29 09:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*toggle nav btn*/
+
 console.log('Hello Dcoder')
 let menu = document.getElementById('menu-btn');
 let close = document.getElementById('close-btn');
@@ -11,3 +13,19 @@ close.addEventListener("click",()=>{
   nav[0].style.opacity = 0;
   nav[0].style.zIndex = -1;
 })
+
+/*toggle end*/
+/*about section tabs*/
+const aboutsection = document.querySelector(".about-section");
+const tabcontainer = document.querySelector(".about-tabs");
+tabcontainer.addEventListener("click",(e)=>{
+  if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+    const target = event.target.getAttribute("data-target");
+    tabcontainer.querySelector(".active").classList.remove("outer-shadow","active");
+    event.target.classList.add("active","outer-shadow");
+    aboutsection.querySelector(".tab-content.active").classList.remove("active");
+    aboutsection.querySelector(target).classList.add("active");
+    
+  }
+})
+
