1638972abd0aaeea24b549730eaccb18bd9eae22
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2021-10-29 17:39:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ close.addEventListener("click",()=>{
 
 /*toggle end*/
 /*about section tabs*/
-
 const aboutsection = document.querySelector(".about-section");
 const tabcontainer = document.querySelector(".about-tabs");
 tabcontainer.addEventListener("click",(e)=>{
@@ -29,3 +28,102 @@ tabcontainer.addEventListener("click",(e)=>{
   }
 })
 
+function scrollingtoggle(){
+  document.body.classList.toggle("stop-scrolling");
+}
+
+
+/* project filter and popup*/
+  
+const filterContainer = document.querySelector(".project-filter");
+const projectItemsContainer = document.querySelector(".project-items");
+const projectItems = document.querySelectorAll(".project-item");
+const popup = document.querySelector(".project-popup");
+const prevBtn = popup.querySelector(".pp-prev");
+const nextBtn = popup.querySelector(".pp-next");
+const closeBtn = popup.querySelector(".pp-close");
+const projectDetailsContainer = popup.querySelector(".pp-details");
+const projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+let itemIndex, slideIndex, screenshots;
+
+/*filter project items*/
+
+filterContainer.addEventListener("click",(e)=>{
+  if(e.target.classList.contains("filter-item") && !e.target.classList.contains("active")){
+   filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+   e.target.classList.add("active","outer-shadow");
+   const target = e.target.getAttribute("data-target");
+   projectItems.forEach((item)=>{
+     if(target===item.getAttribute("data-category") || target==="All"){
+       item.classList.remove("hide");
+       item.classList.add("show");
+     }
+     else{
+       item.classList.remove("show");
+       item.classList.add("hide");
+     }
+     
+   })
+   }
+})
+
+projectItemsContainer.addEventListener("click",(e)=>{
+  if(e.target.closest(".project-item-inner")){
+    const projectItem = e.target.closest(".project-item-inner").parentElement;
+  itemIndex = Array.from(projectItem.parentElement.children).indexOf(projectItem);
+  screenshots = projectItems[itemIndex].querySelector(".project-item-img img").getAttribute("data-screenshots");
+  screenshots = screenshots.split(",");
+  if(screenshots.length === 1){
+    prevBtn.style.display = "none";
+    nextBtn.style.display = "none";
+  }
+  else{
+    prevBtn.style.display = "block";
+    nextBtn.style.display = "block";
+  }
+  slideIndex = 0;
+  popuptoggle();
+  popupslideshow();
+  popupDetails();
+  }
+})
+
+closeBtn.addEventListener("click",()=>{
+  popuptoggle();
+})
+
+function popuptoggle(){
+  popup.classList.toggle("open");
+  scrollingtoggle();
+}
+
+function popupslideshow(){
+  const imgsrc = screenshots[slideIndex];
+  const popupImg = popup.querySelector(".pp-img");
+  popup.querySelector(".pp-loader").classList.add("active");
+  popupImg.src = imgsrc;
+  popupImg.onload = ()=>{
+    popup.querySelector(".pp-loader").classList.remove("active");
+  }
+  popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + " of " + screenshots.length;
+}
+
+nextBtn.addEventListener("click",()=>{
+  if (slideIndex === screenshots.length-1){
+    slideIndex = 0;
+  }
+  else {
+    slideIndex++;
+  }
+  popupslideshow();
+})
+
+prevBtn.addEventListener("click",()=>{
+  if (slideIndex === 0){
+    slideIndex = screenshots.length-1;
+  }
+  else {
+    slideIndex--;
+  }
+  popupslideshow();
+})
