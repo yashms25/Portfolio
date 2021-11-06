c2e27f1af9c2444376bc113b8bee4cf58a09a029
unknown <yashmistry614@gmail.com>
2021-11-06 17:09:49 +0530

    all

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ console.log('Hello Dcoder')
 let menu = document.getElementById('menu-btn');
 let close = document.getElementById('close-btn');
 let nav = document.getElementsByClassName("nav-menu");
-menu.addEventListener("click",()=>{
+menu.addEventListener("click", () => {
   console.log("click menu")
   nav[0].style.opacity = 1;
   nav[0].style.zIndex = 999;
 })
-close.addEventListener("click",()=>{
+close.addEventListener("click", () => {
   nav[0].style.opacity = 0;
   nav[0].style.zIndex = -1;
 })
@@ -18,23 +18,23 @@ close.addEventListener("click",()=>{
 /*about section tabs*/
 const aboutsection = document.querySelector(".about-section");
 const tabcontainer = document.querySelector(".about-tabs");
-tabcontainer.addEventListener("click",(e)=>{
-  if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+tabcontainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
     const target = e.target.getAttribute("data-target");
-    tabcontainer.querySelector(".active").classList.remove("outer-shadow","active");
-    e.target.classList.add("active","outer-shadow");
+    tabcontainer.querySelector(".active").classList.remove("outer-shadow", "active");
+    e.target.classList.add("active", "outer-shadow");
     aboutsection.querySelector(".tab-content.active").classList.remove("active");
     aboutsection.querySelector(target).classList.add("active");
   }
 })
 
-function scrollingtoggle(){
+function scrollingtoggle() {
   document.body.classList.toggle("stop-scrolling");
 }
 
 
 /* project filter and popup*/
-  
+
 const filterContainer = document.querySelector(".project-filter");
 const projectItemsContainer = document.querySelector(".project-items");
 const projectItems = document.querySelectorAll(".project-item");
@@ -48,68 +48,68 @@ let itemIndex, slideIndex, screenshots;
 
 /*filter project items*/
 
-filterContainer.addEventListener("click",(e)=>{
-  if(e.target.classList.contains("filter-item") && !e.target.classList.contains("active")){
-   filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
-   e.target.classList.add("active","outer-shadow");
-   const target = e.target.getAttribute("data-target");
-   projectItems.forEach((item)=>{
-     if(target===item.getAttribute("data-category") || target==="All"){
-       item.classList.remove("hide");
-       item.classList.add("show");
-     }
-     else{
-       item.classList.remove("show");
-       item.classList.add("hide");
-     }
-     
-   })
-   }
+filterContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("filter-item") && !e.target.classList.contains("active")) {
+    filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+    e.target.classList.add("active", "outer-shadow");
+    const target = e.target.getAttribute("data-target");
+    projectItems.forEach((item) => {
+      if (target === item.getAttribute("data-category") || target === "All") {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      }
+      else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+
+    })
+  }
 })
 
-projectItemsContainer.addEventListener("click",(e)=>{
-  if(e.target.closest(".project-item-inner")){
+projectItemsContainer.addEventListener("click", (e) => {
+  if (e.target.closest(".project-item-inner")) {
     const projectItem = e.target.closest(".project-item-inner").parentElement;
-  itemIndex = Array.from(projectItem.parentElement.children).indexOf(projectItem);
-  screenshots = projectItems[itemIndex].querySelector(".project-item-img img").getAttribute("data-screenshots");
-  screenshots = screenshots.split(",");
-  if(screenshots.length === 1){
-    prevBtn.style.display = "none";
-    nextBtn.style.display = "none";
-  }
-  else{
-    prevBtn.style.display = "block";
-    nextBtn.style.display = "block";
-  }
-  slideIndex = 0;
-  popuptoggle();
-  popupslideshow();
-  popupDetails();
+    itemIndex = Array.from(projectItem.parentElement.children).indexOf(projectItem);
+    screenshots = projectItems[itemIndex].querySelector(".project-item-img img").getAttribute("data-screenshots");
+    screenshots = screenshots.split(",");
+    if (screenshots.length === 1) {
+      prevBtn.style.display = "none";
+      nextBtn.style.display = "none";
+    }
+    else {
+      prevBtn.style.display = "block";
+      nextBtn.style.display = "block";
+    }
+    slideIndex = 0;
+    popuptoggle();
+    popupslideshow();
+    popupDetails();
   }
 })
 
-closeBtn.addEventListener("click",()=>{
+closeBtn.addEventListener("click", () => {
   popuptoggle();
 })
 
-function popuptoggle(){
+function popuptoggle() {
   popup.classList.toggle("open");
   scrollingtoggle();
 }
 
-function popupslideshow(){
+function popupslideshow() {
   const imgsrc = screenshots[slideIndex];
   const popupImg = popup.querySelector(".pp-img");
   popup.querySelector(".pp-loader").classList.add("active");
   popupImg.src = imgsrc;
-  popupImg.onload = ()=>{
+  popupImg.onload = () => {
     popup.querySelector(".pp-loader").classList.remove("active");
   }
-  popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + " of " + screenshots.length;
+  popup.querySelector(".pp-counter").innerHTML = (slideIndex + 1) + " of " + screenshots.length;
 }
 
-nextBtn.addEventListener("click",()=>{
-  if (slideIndex === screenshots.length-1){
+nextBtn.addEventListener("click", () => {
+  if (slideIndex === screenshots.length - 1) {
     slideIndex = 0;
   }
   else {
@@ -118,12 +118,43 @@ nextBtn.addEventListener("click",()=>{
   popupslideshow();
 })
 
-prevBtn.addEventListener("click",()=>{
-  if (slideIndex === 0){
-    slideIndex = screenshots.length-1;
+prevBtn.addEventListener("click", () => {
+  if (slideIndex === 0) {
+    slideIndex = screenshots.length - 1;
   }
   else {
     slideIndex--;
   }
   popupslideshow();
 })
+
+function popupDetails() {
+  const details = projectItems[itemIndex].querySelector(".project-item-details").innerHTML;
+  popup.querySelector(".pp-project-details").innerHTML = details;
+  const title = projectItems[itemIndex].querySelector(".project-item-title").innerHTML;
+  popup.querySelector(".pp-title h2").innerHTML = title;
+  const category = projectItems[itemIndex].getAttribute("data-category");
+  popup.querySelector(".pp-project-category").innerHTML = category;
+  
+}
+
+projectDetailsBtn.addEventListener("click", () => {
+  popupDetailsToggle();
+})
+
+function popupDetailsToggle() {
+  if (projectDetailsContainer.classList.contains('active')) {
+    projectDetailsBtn.querySelector("i").classList.remove("fa-minus");
+    projectDetailsBtn.querySelector("i").classList.add("fa-plus");
+    projectDetailsContainer.classList.remove("active");
+    projectDetailsContainer.style.maxHeight = 0 + "px";
+  }
+  else {
+    projectDetailsContainer.classList.add("active");
+    projectDetailsBtn.querySelector("i").classList.add("fa-minus");
+    projectDetailsBtn.querySelector("i").classList.remove("fa-plus");
+    projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px";
+    popup.scrollTo(0, projectDetailsContainer.offsetTop);
+  }
+}
+
