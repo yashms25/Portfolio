4818a44e069e5738793c04c7e6ceb3150ecfba82
yashms25 <yashmistry614@gmail.com>
2021-11-12 17:49:44 +0530

    responsive

--- index.js
+++ index.js
@@ -204,10 +204,10 @@ function popupDetailsToggle() {
 }
 
 // hide all sections except active
-const sections = document.querySelectorAll(".section");
-sections.forEach((section) => {
-  if (!section.classList.contains("active")) {
-    section.classList.add("hide");
-  }
-})
+// const sections = document.querySelectorAll(".section");
+// sections.forEach((section) => {
+//   if (!section.classList.contains("active")) {
+//     section.classList.add("hide");
+//   }
+// })
 
